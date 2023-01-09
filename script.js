61675e5571ce50c6f1a4f7219d31ebe8fa893248
Nouman Afridi <79275961+noman1231@users.noreply.github.com>
2023-01-09 07:10:20 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var menuIcon = document.querySelector(".menu");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+
+menuIcon.onclick = function(){
+    let item =0;
+    sidebar.classList.toggle("small-sidebar")
+    container.classList.toggle("large-container")
+}
+
+//var NotifIcon = document.querySelector(".")
+var 
+let
+const item = 0;
